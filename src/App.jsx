b73339097e64f07fbe5b321ce3dc0689bf390a21
Marcus Neo Rangel <marcusneorangel23@gmail.com>
2026-01-29 16:00:53 +0800

    Initialization of git

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
