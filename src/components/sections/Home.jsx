6784cf45007613d392a928e6ff22886cfc18e869
Marcus Neo Rangel <marcusneorangel23@gmail.com>
2026-02-03 11:49:46 +0800

    Changed the gradient color of the name

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,7 +5,7 @@ export const HomePage = () => {
       className="min-h-screen flex items-center justify-center relative"
     >
       <div className="text-center z-10 px-4 ">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-linear-to-r from-blue-500 to bg-purple-600 bg-clip-text text-transparent leading-right">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-linear-to-r from-blue-500 to bg-cyan-400 bg-clip-text text-transparent leading-right">
           Hi, I'm Marcus Neo!
         </h1>
 
