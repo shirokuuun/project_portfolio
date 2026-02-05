6c635d0fbe2c2f0852279a644bdda4b2eb690050
Marcus Neo Rangel <marcusneorangel23@gmail.com>
2026-02-05 10:09:40 +0800

    Added the reveal on scroll for these sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { NavBar } from "./components/NavBar.jsx";
 import { MobileMenu } from "./components/MobileMenu.jsx";
 import { HomePage } from "./components/sections/Home.jsx";
 import { About } from "./components/sections/About.jsx";
+import { Projects } from "./components/sections/Projects.jsx";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -20,6 +21,7 @@ function App() {
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <HomePage />
         <About />
+        <Projects />
       </div>
     </>
   );
--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,5 @@
+import { RevealOnScroll } from "../RevealOnScroll.jsx";
+
 export const About = () => {
   const frontendSkills = [
     "React",
@@ -15,82 +17,82 @@ export const About = () => {
       id="about"
       className="min-h-screen flex items-center justify-center py-20"
     >
-      <div className="max-w-3xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-linear-to-r from-blue-500 to bg-cyan-400 bg-clip-text text-transparent text-center">
-          About Me
-        </h2>
-        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-          <p className="text-gray-300 mb-6">
-            Motivated developer to apply expertise in software development, app
-            development, and cloud engineering, contribute innovative solutions
-            to real-world challenges, and gain hands-on experience that sharpens
-            technical skills and professional growth.
-          </p>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Frontend</h3>
-              <div className="flex flex-wrap gap-2">
-                {frontendSkills.map((skill, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
-                  >
-                    {skill}
-                  </span>
-                ))}
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-linear-to-r from-blue-500 to bg-cyan-400 bg-clip-text text-transparent text-center">
+            About Me
+          </h2>
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Motivated developer to apply expertise in software development,
+              app development, and cloud engineering, contribute innovative
+              solutions to real-world challenges, and gain hands-on experience
+              that sharpens technical skills and professional growth.
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((skill, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                    >
+                      {skill}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((skill, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                    >
+                      {skill}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Backend</h3>
-              <div className="flex flex-wrap gap-2">
-                {backendSkills.map((skill, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
-                  >
-                    {skill}
-                  </span>
-                ))}
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Education</h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> BS Information Technology</strong> - De La Salle Lipa
+                  (2022-2026)
+                </li>
+                <li>
+                  Relevant Coursework: Web Development, Mobile App Development,
+                  UI/UX Design
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Work Experience</h3>
+              <div className="space-y-4 text-gray-300 ">
+                <div>
+                  <h4 className="font-semibold">
+                    Web Developer & Quality Assurance Intern at Highly Succeed
+                    Inc. (2026-Present)
+                  </h4>
+                  <p>
+                    Assisted in developing an Alumni Website and performed
+                    quality assurance testing to ensure functionality and user
+                    experience to Makati Waste.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4">Education</h3>
-            <ul className="list-disc list-inside text-gray-300 space-y-2">
-              <li>
-                <strong> BS Information Technology</strong> - De La Salle Lipa
-                (2022-2026)
-              </li>
-              <li>
-                Relevant Coursework: Web Development, Mobile App Development,
-                UI/UX Design
-              </li>
-            </ul>
-          </div>
-          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4">Work Experience</h3>
-            <div className="space-y-4 text-gray-300 ">
-              <div>
-                <h4 className="font-semibold">
-                  Web Developer & Quality Assurance Intern at Highly Succeed
-                  Inc. (2026-Present)
-                </h4>
-                <p>
-                  Assisted in developing an Alumni Website and performed quality
-                  assurance testing to ensure functionality and user experience
-                  to Makati Waste.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
 };
